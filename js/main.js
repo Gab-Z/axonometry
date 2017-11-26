060086f7fde34f070e155ae03721ad4cec6822d0
Gab-Z <glevassor@gmail.com>
2017-11-26 14:22:17 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ var Store = {};
 window.onload = () => {
   Store.renderer = new renderer();
   document.body.appendChild( Store.renderer.viewport )
+  addWheelListener( document.body, function( e ) { console.log( e.deltaY ); e.preventDefault(); } );
 }
